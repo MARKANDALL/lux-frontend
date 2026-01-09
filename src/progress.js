2932389a4110b4bf8a659fc4cec514ae26af2b45
Mark Huguley <markwhitehuge@gmail.com>
2026-01-09 01:09:25 -0500

    ui: unify nav pills + add ripple boot on progress page

--- src/progress.js
+++ src/progress.js
@@ -1,6 +1,10 @@
 // src/progress.js
 import { ensureUID } from "../api/identity.js";
 import { initDashboard } from "../features/dashboard/index.js";
+import { bootRippleButtons } from "../ui/ui-ripple-filter.js";
 
 ensureUID();
 initDashboard();
+
+// Enable ripple effect for any element with [data-lux-ripple] on this page
+bootRippleButtons();
