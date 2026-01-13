7ffdf0467bca4383990e1180c3a3d74e67808989
Mark Huguley <markwhitehuge@gmail.com>
2026-01-13 10:22:25 -0500

    convo: add picker scenario media assets (images + videos)

--- features/convo/scenarios.js
+++ features/convo/scenarios.js
@@ -61,4 +61,157 @@ export const SCENARIOS = [
   img: "/convo-img/networking.webp",
   thumb: "/convo-img/networking.webp",
 },
+  {
+    id: "banking",
+    title: "Banking",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/banking.webp",
+    thumb: "/convo-img/banking.webp",
+    video: "/convo-vid/banking.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "calling",
+    title: "Calling",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/calling.webp",
+    thumb: "/convo-img/calling.webp",
+    video: "/convo-vid/calling.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "car",
+    title: "Car",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/car.jpg",
+    thumb: "/convo-img/car.jpg",
+    video: "/convo-vid/car.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "choosing",
+    title: "Choosing",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/choosing.webp",
+    thumb: "/convo-img/choosing.webp",
+    video: "/convo-vid/choosing.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "concern",
+    title: "Concern",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/concern.webp",
+    thumb: "/convo-img/concern.webp",
+    video: "/convo-vid/concern.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "couple",
+    title: "Couple",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/couple.jpg",
+    thumb: "/convo-img/couple.jpg",
+    video: "/convo-vid/couple.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "dinner",
+    title: "Dinner",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/dinner.webp",
+    thumb: "/convo-img/dinner.webp",
+    video: "/convo-vid/dinner.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "joke",
+    title: "Joke",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/joke.jpg",
+    thumb: "/convo-img/joke.jpg",
+    video: "/convo-vid/joke.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "lifeguard",
+    title: "Lifeguard",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/lifeguard.webp",
+    thumb: "/convo-img/lifeguard.webp",
+    video: "/convo-vid/lifeguard.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "mail",
+    title: "Mail",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/mail.jpg",
+    thumb: "/convo-img/mail.jpg",
+    video: "/convo-vid/mail.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "networking",
+    title: "Networking",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/networking.webp",
+    thumb: "/convo-img/networking.webp",
+    video: "/convo-vid/networking.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "parking",
+    title: "Parking",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/parking.webp",
+    thumb: "/convo-img/parking.webp",
+    video: "/convo-vid/parking.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "police",
+    title: "Police",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/police.webp",
+    thumb: "/convo-img/police.webp",
+    video: "/convo-vid/police.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "shopping",
+    title: "Shopping",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/shopping.jpg",
+    thumb: "/convo-img/shopping.jpg",
+    video: "/convo-vid/shopping.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "student",
+    title: "Student",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/student.webp",
+    thumb: "/convo-img/student.webp",
+    video: "/convo-vid/student.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "technology",
+    title: "Technology",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/technology.webp",
+    thumb: "/convo-img/technology.webp",
+    video: "/convo-vid/technology.mp4",
+    more: "TODO: optional details"
+  },
+  {
+    id: "understanding",
+    title: "Understanding",
+    desc: "TODO: one-sentence hook",
+    img: "/convo-img/understanding.webp",
+    thumb: "/convo-img/understanding.webp",
+    video: "/convo-vid/understanding.mp4",
+    more: "TODO: optional details"
+  },
 ];
