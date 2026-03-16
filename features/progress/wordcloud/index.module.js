673ce2f6c30a478094ab7fa3ae25bdf45f855bdb
MARKANDALL <markwhitehuge@gmail.com>
2026-03-16 16:03:54 -0400

    Delete obsolete compatibility files

--- features/progress/wordcloud/index.module.js
+++
@@ -1,4 +0,0 @@
-export { createWordcloudContext } from "./context.js";
-export { getWordcloudDom } from "./dom.js";
-export { bindWordcloudEvents } from "./events.js";
-export { renderWordcloudView } from "./render.js";
